46509fb30528570abdc28ece16334376304ffd1f
Nicolas Kim <kimnicolasalejandro@gmail.com>
2024-07-20 17:09:27 -0300

    commit video card created

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,35 +1,28 @@
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
 import React, {useState} from 'react'
 import { icons } from '../constants'
-import CustomButton from './CustomButton'
 const SearchInput = ({title,value,placeholder,handleChangeText,otherStyles, ...props}) => {
     const [showPassword, setshowPassword] = useState(false)
   return (
-    <View className={`space-y-2 ${otherStyles}`}>
-      <Text className="text=base text-gray-100 font-pmedium">{title}</Text>
-      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
         <TextInput
-        className="flex-1 text-white font-psemibold text-base"
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
         value={value}
-        placeholder={placeholder}
+        placeholder="Search for a video"
         placeholderTextColor="#7b7b8b"
         onChangeText={handleChangeText}
         secureTextEntry={title === 'Password' && !showPassword}
         />
-        {title=== 'Password' && (
-            <TouchableOpacity onPress={()=> setshowPassword(!showPassword)}>
-                <Image
-                source={!showPassword ? icons.eye : icons.eyeHide}
-                className="w-6 h-6"
-                resizeMode='contain'
-                />
-
-            </TouchableOpacity>
-        )}
+       <TouchableOpacity>
+        <Image
+        source={icons.search}
+        className="w-5 h-5"
+        resizeMode='contain'
+        />
+       </TouchableOpacity>
 
       </View>
-    </View>
   )
 }
 
-export default SearchInput
+export default SearchInput;
---
+++ components/Trending.jsx
@@ -0,0 +1,74 @@
+import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native'
+import * as Animatable from 'react-native-animatable'
+import React,{ useState} from 'react'
+import { icons } from '../constants'
+
+const zoomIn ={
+    0:{
+        sacel:0.9
+    },
+    1:{
+        scale:1,
+    }
+    
+}
+const zoomOut ={
+    0:{
+        sacel:1
+    },
+    1:{
+        scale:0.9,
+    }
+    
+}
+
+const TrendingItem = ({activeItem, item})=>{
+    const [play, setPlay] = useState(false)
+console.log(activeItem.$id,item.$id);
+    return(
+        <Animatable.View className="mr-5" animation={activeItem === item.$id ? zoomIn : zoomOut}
+        duration={500}
+        >
+        {play ? (
+            <Text className="text-white">Playing</Text>
+        ):(
+            <TouchableOpacity className="relative justify-center items-center" activeOpacity={0.7} onPress={()=>setPlay(true)}>
+                <ImageBackground
+                    source={{
+                        uri:item.thumbnail
+                    }}
+                    className="w-52 h-72 my-5 overflow-hidden shadow-lg shadow-black/40"
+                    resizeMode='cover'
+                />
+                <Image
+                className="w-12 h-12 absolute"
+                source={icons.play}
+                resizeMode='contain'
+                />
+
+            </TouchableOpacity>
+        )}
+        </Animatable.View>
+    )
+}
+
+const Trending = ({posts}) => {
+    const [activeItem, setActiveItem] = useState(posts[0])
+
+  return (
+    <FlatList
+        data={posts}
+        keyExtractor={(item)=>item.$id}
+        renderItem={({item})=>(
+            <TrendingItem  activeItem={activeItem} item={item}/>
+        )
+    }
+    horizontal
+    >
+
+
+    </FlatList>
+  )
+}
+
+export default Trending
